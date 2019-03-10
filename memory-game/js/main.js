26e141d971cdec001d8c730f0e705f504dfac3c1
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-03-10 17:52:38 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -22,6 +22,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function() {
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -30,15 +31,26 @@ var checkForMatch = function() {
 	}
 }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i <= 4; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		cardElement.appendChild('game-board');
+	}
+}
+createBoard();
+
+
 
 
 
